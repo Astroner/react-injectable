b6806c2ebaf61e81b291dd884c96356f5ea2771d
Evgeny Nikiforov <enikiforov@devexperts.com>
2022-02-21 02:49:31 +0300

    useService hook

---
+++ src/useService.ts
@@ -0,0 +1,14 @@
+import { useContext, useEffect, useMemo, useState } from "react"
+import { Injectable } from "./Injectable";
+import { InjectableContext } from "./InjectableContext"
+import { InjectableConstructor } from "./types"
+
+export const useInjector = <T extends Injectable<any>>(toGet: InjectableConstructor<T>): T => {
+    const ctx = useContext(InjectableContext);
+
+    const service = useMemo(() => {
+        return ctx.map.get(toGet);
+    }, [ctx, toGet])
+
+    return service as T;
+}
